20f078769dc3916830ee067145b18a35971e9c8e
H3ctorOS <h3ctor.oss@gmail.com>
2026-02-12 18:02:37 +0000

    se envia si es la semana actual

--- composeApp/src/commonMain/kotlin/tallerwapo/core/dominio/dto/calendario/SemanaSelectorDTO.kt
+++ composeApp/src/commonMain/kotlin/tallerwapo/core/dominio/dto/calendario/SemanaSelectorDTO.kt
@@ -5,5 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SemanaSelectorDTO(
     val numeroSemana: Int,
-    val anio: Int
+    val anio: Int,
+    val esActual: Boolean // True si es la semana actual
 )
